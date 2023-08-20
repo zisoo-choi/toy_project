38e785268dfe133374fcbe701d95c79cf5e6d6ef
zisoo <zisoo_3@naver.com>
2023-08-21 01:44:14 +0900

    feat: [최지수] 카카오 OAuth 실습 - 0

--- spring/src/main/java/com/example/demo/signUp/controller/SignUpController.java
+++ spring/src/main/java/com/example/demo/signUp/controller/SignUpController.java
@@ -4,10 +4,8 @@ import com.example.demo.signUp.controller.form.SignUpRequestForm;
 import com.example.demo.signUp.service.SignUpService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class SignUpController {
 
-     private final SignUpService signUpService;
+    private final SignUpService signUpService;
 
     @PostMapping("/sign-up")
     public boolean signUp(@RequestBody SignUpRequestForm requestForm) {
@@ -23,4 +21,22 @@ public class SignUpController {
 
         return signUpService.register(requestForm);
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "");
+        return "pages/login/loginPage";
+    }
+
+//    @GetMapping("/kakao")
+//    public ResponseEntity<ApiResponse<Object>> kakaoLogin(@RequestParam String code) {
+//        String answer = "";
+//        log.debug("[+| Kakao Login AccessToken :: " + code);
+//        ApiResponse<Object> ar = ApiResponse
+//                .builder()
+//                .result(answer)
+//                .resultCode(SUCCESS_CODE)
+//                .resultMsg(SUCCESS_MSG).build();
+//        return new ResponseEntity<>(ar, HttpStatus.OK);
+//    }
 }
